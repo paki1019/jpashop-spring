0c068e7cc68d17ddb227986ff5a503c5719215d5
paki1019 <paki1019@gmail.com>
2021-04-11 14:39:04 +0900

    프로젝트 환경설정

---
+++ src/main/java/jpabook/jpashopspring/Hello.java
@@ -0,0 +1,11 @@
+package jpabook.jpashopspring;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+
+    String data;
+}
---
+++ src/test/java/jpabook/jpashopspring/JpashopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
