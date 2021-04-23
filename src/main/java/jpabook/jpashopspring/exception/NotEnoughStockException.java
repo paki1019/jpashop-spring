8361f98bc957b9d7d6fc378435b9ad070e880228
paki1019 <paki1019@gmail.com>
2021-04-24 00:15:47 +0900

    상품 도메인 개발

---
+++ src/main/java/jpabook/jpashopspring/exception/NotEnoughStockException.java
@@ -0,0 +1,19 @@
+package jpabook.jpashopspring.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+}
